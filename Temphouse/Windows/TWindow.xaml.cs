9dd19bffe28e1b22d0768739683358b8bb62d5a2
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-03-27 22:25:17 +0500

    Создан класс, наследуемый от окна, но он не отображается в дизайнере.

---
+++ Temphouse/Windows/TWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Temphouse.Themes.ExtendedWindowControl;
+
+namespace Temphouse.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для TWindow.xaml
+    /// </summary>
+    public partial class TWindow : ExtendedWindow
+    {
+        public TWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
