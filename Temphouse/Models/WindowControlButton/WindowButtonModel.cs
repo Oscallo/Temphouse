7213fda1948219753c86cf051fae8e98faa20086
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-05-02 20:36:06 +0500

    Заготовка под наполнение кнопок по аналогии с ExtendedSystemCommands.

---
+++ Temphouse/Models/WindowControlButton/WindowButtonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temphouse.Models.WindowControlButton
+{
+    public class WindowButtonModel
+    {
+        public WindowButtonModel() { }  
+
+        public object? Content { get; set; }
+    }
+}
---
+++ Temphouse/Modules/Filling/WindowButtonFilling.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Temphouse.Models.WindowControlButton;
+
+namespace Temphouse.Modules.Filling
+{
+    public static class WindowButtonFilling
+    {
+        private static WindowButtonModel _WindowCloseButtonFilling = new WindowButtonModel();
+
+        private static WindowButtonModel _WindowMinimizeButtonFilling = new WindowButtonModel();
+
+        private static WindowButtonModel _WindowMaximizeButtonFilling = new WindowButtonModel();
+
+        private static WindowButtonModel _WindowHideButtonFilling = new WindowButtonModel();
+
+        private static WindowButtonModel _WindowRestoreButtonFilling = new WindowButtonModel();
+
+        public static WindowButtonModel WindowCloseButtonFilling 
+        {
+            get { return _WindowCloseButtonFilling; }
+            private set 
+            {
+                _WindowCloseButtonFilling = value;
+            }
+        }
+
+        public static WindowButtonModel WindowMinimizeButtonFilling
+        {
+            get { return _WindowMinimizeButtonFilling; }
+            private set
+            {
+                _WindowMinimizeButtonFilling = value;
+            }
+        }
+
+        public static WindowButtonModel WindowMaximizeButtonFilling
+        {
+            get { return _WindowMaximizeButtonFilling; }
+            private set
+            {
+                _WindowMaximizeButtonFilling = value;
+            }
+        }
+
+        public static WindowButtonModel WindowHideButtonFilling
+        {
+            get { return _WindowHideButtonFilling; }
+            private set
+            {
+                _WindowHideButtonFilling = value;
+            }
+        }
+
+        public static WindowButtonModel WindowRestoreButtonFilling
+        {
+            get { return _WindowRestoreButtonFilling; }
+            private set
+            {
+                _WindowRestoreButtonFilling = value;
+            }
+        }
+
+        static WindowButtonFilling() 
+        {
+
+        }
+    }
+}
