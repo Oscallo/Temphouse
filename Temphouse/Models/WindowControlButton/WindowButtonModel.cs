83da8213d6b76a730509c1ee0d7d818f98fdd186
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-05-02 20:40:33 +0500

    INotifyPropertyChanged и INotifyPropertyChanging для WindowButtonModel.

--- Temphouse/Models/WindowControlButton/WindowButtonModel.cs
+++ Temphouse/Models/WindowControlButton/WindowButtonModel.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Temphouse.Models.WindowControlButton
 {
-    public class WindowButtonModel
+    public class WindowButtonModel : INotifyPropertyChanged, INotifyPropertyChanging
     {
-        public WindowButtonModel() { }  
+        public WindowButtonModel() { }
 
-        public object? Content { get; set; }
+        private object? _Content;
+
+        public object? Content 
+        {
+            get { return _Content; }
+            set 
+            {
+                OnPropertyChanging(nameof(this.Content));
+                _Content = value;
+                OnPropertyChanged(nameof(this.Content));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public event PropertyChangingEventHandler? PropertyChanging;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public void OnPropertyChanging([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanging != null)
+                PropertyChanging(this, new PropertyChangingEventArgs(prop));
+        }
     }
 }
