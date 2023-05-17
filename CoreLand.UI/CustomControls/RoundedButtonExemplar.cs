e671499485cc98e8ee55f523c16f367b6fd008ef
Oscallo <oscallo.developer@gmail.com>
2023-05-17 16:59:08 +0500

    Выделение всех графических элементов в отдельную библиотеку.

---
+++ CoreLand.UI/CustomControls/RoundedButtonExemplar.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace CoreLand.UI.CustomControls
+{
+    public partial class RoundedButton : SquaredButton
+    {
+        /// <summary>
+        /// Свойство CornerRadius позволяет пользователям независимо управлять округлостью углов, устанавливая 
+        /// значение радиуса для каждого угла. Слишком большие значения радиуса масштабируются таким образом, 
+        /// чтобы они плавно переходят из угла в угол.
+        /// </summary>
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+
+        }
+    }
+}
---
+++ CoreLand.UI/CustomControls/SquaredButtonDependency.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CoreLand.UI.CustomControls
+{
+    public partial class SquaredButton : Button
+    {
+        /// <summary>
+        /// <seealso cref="DependencyProperty"/> задающее ширину и высоту кнопки.
+        /// </summary>
+        public static readonly DependencyProperty EdgeProperty = DependencyProperty.Register(
+                                                                 "Edge", typeof(double), typeof(SquaredButton),
+                                                                 new FrameworkPropertyMetadata(Double.NaN, FrameworkPropertyMetadataOptions.AffectsMeasure, new PropertyChangedCallback(OnTransformDirty)),
+                                                                 new ValidateValueCallback(IsEdgeValid));
+
+
+        /// <summary>
+        /// Вызывается при смене <seealso cref="Edge"/>
+        /// </summary>
+        /// <param name="d"><seealso cref="SquaredButton"/></param>
+        /// <param name="e">Аргументы</param>
+        private static void OnTransformDirty(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            SquaredButton squaredButton = ((SquaredButton)d);
+
+            squaredButton.Width = (double)e.NewValue;
+            squaredButton.Height = (double)e.NewValue;
+        }
+
+        /// <summary>
+        /// Проверка на валидность
+        /// </summary>
+        /// <param name="value">Предпологаемый <seealso cref="Edge"/></param>
+        /// <returns></returns>
+        private static bool IsEdgeValid(object value)
+        {
+            double doubleValue = (double)value;
+            return (Double.IsNaN(doubleValue)) || (doubleValue >= 0.0d && !Double.IsPositiveInfinity(doubleValue));
+        }
+
+        static SquaredButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SquaredButton), new FrameworkPropertyMetadata(typeof(SquaredButton)));
+        }
+    }
+}
