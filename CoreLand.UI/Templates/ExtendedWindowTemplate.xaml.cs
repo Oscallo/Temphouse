a3035d843541f5025ea400244115de0f8adea37a
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-06-12 23:18:23 +0500

    Добавление шаблона.

---
+++ CoreLand.UI/Templates/ExtendedWindowTemplate.xaml.cs
@@ -0,0 +1,28 @@
+﻿using CoreLand.UI.CustomControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoreLand.UI.Templates
+{
+    /// <summary>
+    /// Логика взаимодействия для ExtendedWindowTemplate.xaml
+    /// </summary>
+    public partial class ExtendedWindowTemplate : ExtendedWindow
+    {
+        public ExtendedWindowTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
