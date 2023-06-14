3064f14899317169753fe002ed6e140f273c1054
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-06-14 21:53:48 +0500

    Заготовка под избавление от AllowsTransparency=True

---
+++ CoreLand.UI/Enums/Window/WindowStyle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLand.UI.Enums.Window
+{
+    public enum WindowStyle
+    {
+        None,
+        SingleBorderWindow,
+        ThreeDBorderWindow,
+        ToolWindow,
+        HideableSingleBorderWindow,
+        HideableThreeDBorderWindow, 
+        HideableToolWindow
+    }
+}
