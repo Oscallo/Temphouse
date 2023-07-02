417e36e823ad930fb4648ce61cab97db503bac57
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-07-02 16:54:43 +0500

    Заготовка под модели и адаптеры

--- Temphouse/Models/MenuButtonModel.cs
+++ Temphouse/Models/MenuButtonModel.cs
@@ -9,6 +9,7 @@ namespace Temphouse.Models
     public class MenuButtonModel : CommonIconModel, ISelectable
     {
         private bool _IsSelected = false;
+        private string _Title = string.Empty;
 
         public MenuButtonModel() : base() { }
 
@@ -23,6 +24,18 @@ namespace Temphouse.Models
                 OnPropertyChanging(nameof(IsSelected));
                 _IsSelected = value;
                 OnPropertyChanged(nameof(IsSelected));
+            }
+        }
+
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                if (_Title == value) { return; }
+                OnPropertyChanging(nameof(Title));
+                _Title = value;
+                OnPropertyChanged(nameof(Title));
             }
         }
     }
---
+++ Temphouse/Models/UserModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temphouse.Models
+{
+    public class UserBaseModel
+    {
+
+    }
+
+    public class UserModel : UserBaseModel
+    {
+        public UserModel() : base() { }
+    }
+
+    public class UserWithoutPasswordModel : UserBaseModel
+    {
+        public UserWithoutPasswordModel() : base() { }
+    }
+}
