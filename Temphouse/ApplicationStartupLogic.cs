80acca70f1f84ddefbd5137d7714f83f6ef14d65
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-07-09 14:26:24 +0500

    Заготовка под логику загрузки приложения.

---
+++ Temphouse/ApplicationStartupLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temphouse
+{
+    public static class ApplicationStartupLogic
+    {
+        public static Uri StartupWindowUri { get => _GetStartupWindow(); }
+
+        private static Uri _GetStartupWindow()
+        {
+            return new Uri("/Windows/LoginWindow.xaml", UriKind.Relative);
+        }
+    }
+}
