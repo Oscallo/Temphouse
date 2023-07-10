2fe897209a12162ea7a2986e5181a150483f354f
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-07-10 21:25:52 +0500

    DpiHelper

---
+++ CoreLand.UI/Modules/Helpers/DpiHelper.cs
@@ -0,0 +1,101 @@
+﻿using CoreLand.UI.Modules.Designer;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CoreLand.UI.Modules.Helpers
+{
+    /// <summary>
+    /// <seealso cref="https://referencesource.microsoft.com/#PresentationFramework/src/Framework/System/Windows/Standard/DpiHelper.cs,516d4d7a84122f91"/>
+    /// </summary>
+
+    public class DpiHelper
+    {
+        #region static Instance
+
+        /// <summary>
+        /// Статический экземпляр объекта.
+        /// </summary>
+        public static DpiHelper Instance { get; private set; }
+
+        static DpiHelper()
+        {
+            Instance = new DpiHelper();
+        }
+
+        #endregion
+
+        [ThreadStatic]
+        private Matrix _transformToDevice;
+        [ThreadStatic]
+        private Matrix _transformToDip;
+
+        /// <summary>
+        /// Convert a point in device independent pixels (1/96") to a point in the system coordinates.
+        /// </summary>
+        /// <param name="logicalPoint">A point in the logical coordinate system.</param>
+        /// <returns>Returns the parameter converted to the system's coordinates.</returns>
+        public Point LogicalPixelsToDevice(Point logicalPoint, double dpiScaleX, double dpiScaleY)
+        {
+            _transformToDevice = Matrix.Identity;
+            _transformToDevice.Scale(dpiScaleX, dpiScaleY);
+            return _transformToDevice.Transform(logicalPoint);
+        }
+
+        /// <summary>
+        /// Convert a point in system coordinates to a point in device independent pixels (1/96").
+        /// </summary>
+        /// <param name="logicalPoint">A point in the physical coordinate system.</param>
+        /// <returns>Returns the parameter converted to the device independent coordinate system.</returns>
+        public Point DevicePixelsToLogical(Point devicePoint, double dpiScaleX, double dpiScaleY)
+        {
+            _transformToDip = Matrix.Identity;
+            _transformToDip.Scale(1d / dpiScaleX, 1d / dpiScaleY);
+            return _transformToDip.Transform(devicePoint);
+        }
+
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        public Rect LogicalRectToDevice(Rect logicalRectangle, double dpiScaleX, double dpiScaleY)
+        {
+            Point topLeft = LogicalPixelsToDevice(new Point(logicalRectangle.Left, logicalRectangle.Top), dpiScaleX, dpiScaleY);
+            Point bottomRight = LogicalPixelsToDevice(new Point(logicalRectangle.Right, logicalRectangle.Bottom), dpiScaleX, dpiScaleY);
+
+            return new Rect(topLeft, bottomRight);
+        }
+
+        public Rect DeviceRectToLogical(Rect deviceRectangle, double dpiScaleX, double dpiScaleY)
+        {
+            Point topLeft = DevicePixelsToLogical(new Point(deviceRectangle.Left, deviceRectangle.Top), dpiScaleX, dpiScaleY);
+            Point bottomRight = DevicePixelsToLogical(new Point(deviceRectangle.Right, deviceRectangle.Bottom), dpiScaleX, dpiScaleY);
+
+            return new Rect(topLeft, bottomRight);
+        }
+
+        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        public Size LogicalSizeToDevice(Size logicalSize, double dpiScaleX, double dpiScaleY)
+        {
+            Point pt = LogicalPixelsToDevice(new Point(logicalSize.Width, logicalSize.Height), dpiScaleX, dpiScaleY);
+
+            return new Size { Width = pt.X, Height = pt.Y };
+        }
+
+        public Size DeviceSizeToLogical(Size deviceSize, double dpiScaleX, double dpiScaleY)
+        {
+            Point pt = DevicePixelsToLogical(new Point(deviceSize.Width, deviceSize.Height), dpiScaleX, dpiScaleY);
+
+            return new Size(pt.X, pt.Y);
+        }
+
+        public Thickness LogicalThicknessToDevice(Thickness logicalThickness, double dpiScaleX, double dpiScaleY)
+        {
+            Point topLeft = LogicalPixelsToDevice(new Point(logicalThickness.Left, logicalThickness.Top), dpiScaleX, dpiScaleY);
+            Point bottomRight = LogicalPixelsToDevice(new Point(logicalThickness.Right, logicalThickness.Bottom), dpiScaleX, dpiScaleY);
+
+            return new Thickness(topLeft.X, topLeft.Y, bottomRight.X, bottomRight.Y);
+        }
+
+        public DpiScale GetDpi(Visual visual) => VisualTreeHelper.GetDpi(visual);
+
+    }
+}
