6f089b830c48a677e574f85b91840b9fe7a99a5a
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-07-10 22:54:13 +0500

    Добавлена проверка на null.

---
+++ Temphouse/Converters/StringToFirstCharConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Temphouse.Converters
+{
+    /// <summary>
+    /// Конвертер для получения первого <seealso cref="char">символа</seealso> из <seealso cref="string">строки</seealso>
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(char))]
+    public class StringToFirstCharConverter : IValueConverter
+    {
+        private const int _FIRSTCHARCONST = 0;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) { throw new NullReferenceException("Строка равна Null"); }
+            return ((string)value)[_FIRSTCHARCONST];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
