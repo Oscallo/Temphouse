2c4641b41600b5b25cf1e7d1796d82f5888d339a
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-07-14 21:30:24 +0500

    Окно по центру экрана. DataBaseAdapter превратился в патерны Строитель + Фасад.

---
+++ Temphouse/Modules/Adapters/AbstractDataBaseAdapter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Temphouse.Enums;
+using Temphouse.Models;
+
+namespace Temphouse.Modules.Adapters
+{
+    public abstract class AbstractDataBaseAdapter : AbstractAdapter, IDisposable
+    {
+        public DatabaseConnectionStatusEnum DatabaseConnectionStatus { get; set; } = DatabaseConnectionStatusEnum.Closed;
+
+        public AbstractDataBaseAdapter()
+        {
+            OnInitialize();
+        }
+
+        public virtual void Dispose()
+        {
+            this.CloseConnection();
+        }
+
+        public abstract SessionModel UserAuthorization(string login, string password, out bool isAuthorizationSuccess);
+
+        public abstract void UserAuthorization(SessionModel session, out bool isAuthorizationSuccess);
+
+        public abstract UserModel GetUserBySession(SessionModel session);
+
+        public abstract void CreateDataBase();
+
+        public abstract void OnInitialize();
+
+        public virtual void CreateConnection() 
+        {
+            if (DatabaseConnectionStatus == DatabaseConnectionStatusEnum.Opened) { return; }
+            if (DatabaseConnectionStatus == DatabaseConnectionStatusEnum.Opening) { return; }
+        }
+
+        public virtual void CloseConnection() 
+        {
+            if (DatabaseConnectionStatus == DatabaseConnectionStatusEnum.Closed) { return; }
+            if (DatabaseConnectionStatus == DatabaseConnectionStatusEnum.Closing) { return; }
+        }
+    }
+}
