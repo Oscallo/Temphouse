f6c7065da8469235997f1367157b7e67344be709
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-07-14 22:26:40 +0500

    ObservableCollection с вынесеными событиями.

---
+++ CoreLand.UI/Modules/Collections/ActionedObservableCollection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+
+namespace CoreLand.UI.Modules.Collections
+{
+    public class ActionedObservableCollection<T> : ObservableCollection<T>
+    {
+        public event EventHandler<NotifyCollectionChangedEventArgs> ItemAdded;
+        public event EventHandler<NotifyCollectionChangedEventArgs> ItemRemoved;
+        public event EventHandler<NotifyCollectionChangedEventArgs> ItemMoved;
+        public event EventHandler<NotifyCollectionChangedEventArgs> ItemReplased;
+        public event EventHandler<NotifyCollectionChangedEventArgs> ItemReseted;
+
+        public ActionedObservableCollection() 
+        {
+            this.CollectionChanged += (sender, e) =>
+            {
+                if (e.Action == NotifyCollectionChangedAction.Add)
+                {
+                    ItemAdded?.Invoke(this, e);
+                }
+                else if (e.Action == NotifyCollectionChangedAction.Remove)
+                {
+                    ItemRemoved?.Invoke(this, e);
+                }
+                else if (e.Action == NotifyCollectionChangedAction.Move)
+                {
+                    ItemMoved?.Invoke(this, e);
+                }
+                else if (e.Action == NotifyCollectionChangedAction.Replace)
+                {
+                    ItemReplased?.Invoke(this, e);
+                }
+                else if (e.Action == NotifyCollectionChangedAction.Reset)
+                {
+                    ItemReseted?.Invoke(this, e);
+                }
+                else 
+                {
+                    throw new ArgumentOutOfRangeException(nameof(e));
+                }
+            };
+        }
+    }
+}
