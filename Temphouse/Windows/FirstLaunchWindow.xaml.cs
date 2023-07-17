a5c9033639bda5f045f6c23fcdd30fcb77c5255a
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-07-17 23:17:57 +0500

    Добавлены комманды

--- Temphouse/Windows/FirstLaunchWindow.xaml.cs
+++ Temphouse/Windows/FirstLaunchWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Temphouse.Modules.Commands;
 
 namespace Temphouse.Windows
 {
@@ -23,6 +24,29 @@ namespace Temphouse.Windows
         public FirstLaunchWindow()
         {
             InitializeComponent();
+
+            this.CommandBindings.Add(new CommandBinding(FirstLaunchCommands.NextTabItemCommand, OnNextTabItem, OnCanGoToNextTabItem));
+            this.CommandBindings.Add(new CommandBinding(FirstLaunchCommands.BackTabItemCommand, OnBackTabItem, OnCanGoToBackTabItem));
+        }
+
+        private void OnCanGoToBackTabItem(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = FirstLaunchCommands.OnCanGoToBackTabItem(PART_TabControl);
+        }
+
+        private void OnCanGoToNextTabItem(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = FirstLaunchCommands.OnCanGoToNextTabItem(PART_TabControl);
+        }
+
+        private void OnBackTabItem(object sender, ExecutedRoutedEventArgs e)
+        {
+            FirstLaunchCommands.GoToBackTabItem(PART_TabControl);
+        }
+
+        private void OnNextTabItem(object sender, ExecutedRoutedEventArgs e)
+        {
+            FirstLaunchCommands.GoToNextTabItem(PART_TabControl);
         }
     }
 }
