722c6ceddefd224047fdf9748c756262a4068391
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-07-17 23:06:54 +0500

    IsFirstLaunch смена значения.

--- Temphouse/Properties/Settings.Designer.cs
+++ Temphouse/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Temphouse.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool IsFirstLaunch {
             get {
                 return ((bool)(this["IsFirstLaunch"]));
