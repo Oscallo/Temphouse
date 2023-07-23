ffcee0b40bfc63929786d9727e9643b74c8adf1f
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-07-23 23:15:26 +0500

    Получение первого значения из настроек.

--- Temphouse/ViewModels/DataBaseInformationViewModel.cs
+++ Temphouse/ViewModels/DataBaseInformationViewModel.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using Temphouse.Enums;
+using Temphouse.Modules.Adapters;
 
 namespace Temphouse.ViewModels
 {
     public class DataBaseInformationViewModel : FillinableViewModel
     {
-        private DatabaseTypeEnum _DatabaseType = DatabaseTypeEnum.None;
+        private DatabaseTypeEnum _DatabaseType = UserSettingsAdapter.Instance.DatabaseConnectionType;
 
         public DatabaseTypeEnum DatabaseType
         {
