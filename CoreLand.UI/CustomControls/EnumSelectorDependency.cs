96fbb27e3dbc5ad72291025c8c4066e4184b2d38
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-07-26 23:05:42 +0500

    Заготовка под CustomControl

---
+++ CoreLand.UI/CustomControls/EnumSelectorDependency.cs
@@ -0,0 +1,21 @@
+﻿using CoreLand.UI.Modules.Boxes;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace CoreLand.UI.CustomControls
+{
+    public partial class EnumSelector : Selector
+    {
+        static EnumSelector()
+        {
+            KeyboardNavigation.TabNavigationProperty.OverrideMetadata(typeof(EnumSelector), new FrameworkPropertyMetadata(KeyboardNavigationMode.Local));
+            KeyboardNavigation.ControlTabNavigationProperty.OverrideMetadata(typeof(EnumSelector), new FrameworkPropertyMetadata(KeyboardNavigationMode.None));
+            KeyboardNavigation.DirectionalNavigationProperty.OverrideMetadata(typeof(EnumSelector), new FrameworkPropertyMetadata(KeyboardNavigationMode.None));
+
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(EnumSelector), new FrameworkPropertyMetadata(typeof(EnumSelector)));
+        }
+    }
+}
---
+++ CoreLand.UI/CustomControls/EnumSelectorExemplar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Controls.Primitives;
+
+namespace CoreLand.UI.CustomControls
+{
+    public partial class EnumSelector : Selector
+    {
+        public EnumSelector() : base()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+
+        }
+    }
+}
