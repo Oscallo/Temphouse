836a5b204c90384c366c822d7ba3f067057196ab
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-08-02 23:16:50 +0500

    Использование аттрибутов.

--- Temphouse/Enums/DatabaseTypeEnum.cs
+++ Temphouse/Enums/DatabaseTypeEnum.cs
@@ -1,9 +1,20 @@
-﻿namespace Temphouse.Enums
+﻿using CoreLand.UI.Attributes.Enum;
+using System.ComponentModel;
+
+namespace Temphouse.Enums
 {
     public enum DatabaseTypeEnum
     {
+        [Browsable(false)]
+        [Name("Соединение отсутствует")]
         None = 0,
+
+        [Browsable(true)]
+        [Name("Файловая БД")]
         File = 1,
+
+        [Browsable(true)]
+        [Name("Сетевая БД")]
         Service = 2
     }
 }
