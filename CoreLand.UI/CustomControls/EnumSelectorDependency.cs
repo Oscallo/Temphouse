e7d0c3b3ae68e59abf051cc7c5183533b9987a0d
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-08-02 23:19:19 +0500

    EnumTypeProperty

--- CoreLand.UI/CustomControls/EnumSelectorDependency.cs
+++ CoreLand.UI/CustomControls/EnumSelectorDependency.cs
@@ -1,7 +1,7 @@
-﻿using CoreLand.UI.Modules.Boxes;
+﻿using CoreLand.UI.Enums;
+using CoreLand.UI.Modules.Managers;
 using System;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 
@@ -9,6 +9,25 @@ namespace CoreLand.UI.CustomControls
 {
     public partial class EnumSelector : Selector
     {
+        private const DefaultEnum _DefaultEnum = DefaultEnum.Default;
+
+        public static readonly DependencyProperty EnumTypeProperty = DependencyProperty.Register(nameof(EnumType), typeof(Type), typeof(EnumSelector),
+                                                         new FrameworkPropertyMetadata(_DefaultEnum.GetType(), FrameworkPropertyMetadataOptions.AffectsRender, new PropertyChangedCallback(EnumTypeChanged)),new ValidateValueCallback(EnumTypeValidation));
+
+        private static void EnumTypeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            EnumSelector enumSelector = (EnumSelector)d;
+
+            enumSelector.ItemsSource = EnumerationManager.GetValues((Type)e.NewValue);
+        }
+
+        private static bool EnumTypeValidation(object value)
+        {
+            if (value == null) { return false; }
+            if ((((Type)value).BaseType).FullName == "System.Enum") { return true; }
+            return true;
+        }
+
         static EnumSelector()
         {
             KeyboardNavigation.TabNavigationProperty.OverrideMetadata(typeof(EnumSelector), new FrameworkPropertyMetadata(KeyboardNavigationMode.Local));
--- CoreLand.UI/CustomControls/EnumSelectorExemplar.cs
+++ CoreLand.UI/CustomControls/EnumSelectorExemplar.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Collections;
 using System.Windows.Controls.Primitives;
 
 namespace CoreLand.UI.CustomControls
 {
     public partial class EnumSelector : Selector
     {
+        public Type EnumType
+        {
+            get { return (Type)GetValue(EnumTypeProperty); }
+            set { SetValue(EnumTypeProperty, value); }
+        }
+
+        public new IEnumerable ItemsSource 
+        { 
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); } 
+            internal set { SetValue(ItemsSourceProperty, value);}
+        }
+
         public EnumSelector() : base()
         {
             Initialize();
