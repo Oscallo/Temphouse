fb1c6090842b787165071f696704492960d72555
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-08-02 23:15:55 +0500

    EnumAtrribute

---
+++ CoreLand.UI/Attributes/Enum/NameAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CoreLand.UI.Attributes.Enum
+{
+    public class NameAttribute : Attribute
+    {
+        public string Name { get; private set; }
+
+        public NameAttribute(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
