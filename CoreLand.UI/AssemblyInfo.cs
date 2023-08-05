ceda08a28264d6a8f6d5591bde3130706bfe8f8c
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-08-05 12:20:42 +0500

    XmlnsDefinitions: http://development.oscallo.com/coreland/ui/controls http://development.oscallo.com/coreland/ui/mvvm

--- CoreLand.UI/AssemblyInfo.cs
+++ CoreLand.UI/AssemblyInfo.cs
@@ -1,10 +1,15 @@
 using System.Windows;
+using System.Windows.Markup;
 
-[assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-                                     //(used if a resource is not found in the page,
-                                     // or application resource dictionaries)
-    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                              //(used if a resource is not found in the page,
-                                              // app, or any theme specific resource dictionaries)
-)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+
+[assembly: XmlnsDefinition("http://development.oscallo.com/coreland/ui/controls", "CoreLand.UI.CustomControls.Buttons")]
+[assembly: XmlnsDefinition("http://development.oscallo.com/coreland/ui/controls", "CoreLand.UI.CustomControls.HintableBoxes")]
+[assembly: XmlnsDefinition("http://development.oscallo.com/coreland/ui/controls", "CoreLand.UI.CustomControls.Panels")]
+[assembly: XmlnsDefinition("http://development.oscallo.com/coreland/ui/controls", "CoreLand.UI.CustomControls.Selectors")]
+[assembly: XmlnsDefinition("http://development.oscallo.com/coreland/ui/controls", "CoreLand.UI.CustomControls.Windows")]
+
+
+[assembly: XmlnsDefinition("http://development.oscallo.com/coreland/ui/mvvm", "CoreLand.UI.MVVM.Models")]
+[assembly: XmlnsDefinition("http://development.oscallo.com/coreland/ui/mvvm", "CoreLand.UI.MVVM.ViewModels")]
