e500ec9e6d0e284d07ffca8d587b3b57ac80fe15
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-08-19 22:01:50 +0500

    ViewModel для окна

---
+++ Temphouse/MVVM/ViewModels/ApplicationInitializeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CoreLand.UI.Managers.Enumeration;
+using CoreLand.UI.MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Temphouse.DataAccess.Adapters.Subject;
+using Temphouse.DataAccess.Enums;
+
+namespace Temphouse.MVVM.ViewModels
+{
+    public class ApplicationInitializeViewModel : BaseViewModel
+    {
+        public DatabaseTypeEnum DatabaseType
+        {
+            get { return UserSettingsAdapter.Instance.DatabaseConnectionType; }
+            set
+            {
+                if (value == UserSettingsAdapter.Instance.DatabaseConnectionType) { return; }
+                OnPropertyChanging(nameof(DatabaseType));
+                UserSettingsAdapter.Instance.DatabaseConnectionType = value;
+                OnPropertyChanged(nameof(DatabaseType));
+            }
+        }
+
+        public Array DatabaseTypes => EnumerationManager.GetValues(typeof(DatabaseTypeEnum));
+    }
+}
