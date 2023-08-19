205eb79c14117149d13e65f74fbcf549ba63c32c
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-08-19 22:01:32 +0500

    Замена окна инициализации.

--- Temphouse/ApplicationStartupLogic.cs
+++ Temphouse/ApplicationStartupLogic.cs
@@ -4,14 +4,14 @@ using Temphouse.DataAccess.Adapters.Subject;
 namespace Temphouse
 {
     public static class ApplicationStartupLogic
-        {
+    {
         public static Uri StartupWindowUri { get => _GetStartupWindow(); }
 
         private static Uri _GetStartupWindow()
         {
             if (UserSettingsAdapter.Instance.IsFirstLaunch == true)
             {
-                return new Uri("/Windows/FirstLaunchWindow.xaml", UriKind.Relative);
+                return new Uri("/Windows/ApplicationInitializeWindow.xaml", UriKind.Relative);
             }
             else 
             {
