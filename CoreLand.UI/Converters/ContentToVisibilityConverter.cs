98b457c80ac5d0aa3cfb17443228c7edc6396e42
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-09-04 23:06:58 +0500

    Изменения в названии конвертеров и  ContentToVisibilityConverter.

--- CoreLand.UI/Converters/ContentToVisibilityConverter.cs
+++ CoreLand.UI/Converters/ContentToVisibilityConverter.cs
@@ -1,21 +1,22 @@
-﻿using CoreLand.UI.Managers.Enumeration;
-using System;
+﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace CoreLand.UI.Converters
 {
-    [ValueConversion(typeof(Enum), typeof(string))]
-    public class EnumElementToNameString : IValueConverter
+    [ValueConversion(typeof(object), typeof(Visibility))]
+    internal class ContentToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return EnumerationManager.GetNameOfEnum(value,value.GetType());
+            if (value == null) {return Visibility.Collapsed;}
+            else { return Visibility.Visible; }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return EnumerationManager.GetEnumByName(value, targetType);
+            throw new NotSupportedException();
         }
     }
 }
---
+++ CoreLand.UI/Converters/EnumElementToNameStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using CoreLand.UI.Managers.Enumeration;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CoreLand.UI.Converters
+{
+    [ValueConversion(typeof(Enum), typeof(string))]
+    public class EnumElementToNameStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return EnumerationManager.GetNameOfEnum(value,value.GetType());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return EnumerationManager.GetEnumByName(value, targetType);
+        }
+    }
+}
