a491b5e556a12bda642282e4aee4efd32073766f
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-09-08 21:21:01 +0500

    Данные администратора

--- Temphouse/MVVM/ViewModels/ApplicationInitializeViewModel.cs
+++ Temphouse/MVVM/ViewModels/ApplicationInitializeViewModel.cs
@@ -3,12 +3,15 @@ using CoreLand.UI.MVVM.ViewModels;
 using System;
 using Temphouse.DataAccess.Adapters.Subject;
 using Temphouse.DataAccess.Enums;
+using Temphouse.MVVM.Models;
 
 namespace Temphouse.MVVM.ViewModels
 {
     public class ApplicationInitializeViewModel : BaseViewModel
     {
         private bool _IsDatabaseAlredyCreated = false;
+        private UserModel _AdministratorData = new UserModel();
+
         public DatabaseTypeEnum DatabaseType
         {
             get { return UserSettingsAdapter.Instance.DatabaseConnectionType; }
@@ -32,6 +35,13 @@ namespace Temphouse.MVVM.ViewModels
             }
         }
 
-        private bool _IsDatabaseAlredyCreated = false;
+        public UserModel AdministratorData 
+        {
+            get { return _AdministratorData; }
+            set
+            {
+                SetValue(ref _AdministratorData, value);
+            }
+        }
     }
 }
