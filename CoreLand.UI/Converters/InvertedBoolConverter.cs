2d86b7b6cbf4e941fff87b1c80fffd4a3233fce0
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-09-08 19:39:15 +0500

    Инвертирующий конвертер.

---
+++ CoreLand.UI/Converters/InvertedBoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CoreLand.UI.Converters
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InvertedBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+    }
+}
