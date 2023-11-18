135cfdf9f9c6867bad2907fdf046ba7e4913c5db
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-11-18 16:14:01 +0500

    Добавлен Binding, с учетом того, что Password = SecureString.

--- Temphouse/Windows/ApplicationInitializeWindow.xaml.cs
+++ Temphouse/Windows/ApplicationInitializeWindow.xaml.cs
@@ -1,18 +1,7 @@
-﻿using CoreLand.UI.CustomControls;
-using CoreLand.UI.CustomControls.Windows;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CoreLand.UI.CustomControls.Windows;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Temphouse.MVVM.ViewModels;
 
 namespace Temphouse.Windows
 {
@@ -24,6 +13,14 @@ namespace Temphouse.Windows
         public ApplicationInitializeWindow()
         {
             InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext != null)
+            { 
+                ((ApplicationInitializeViewModel)this.DataContext).AdministratorData.Password = ((PasswordBox)sender).SecurePassword;
+            }
         }
     }
 }
