ba176331d0bfa27bdf51964bc33e631a2e586791
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-11-18 14:57:47 +0500

    Создание обратного конвертера

---
+++ CoreLand.UI/Converters/ColorToSolidColorBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CoreLand.UI.Converters
+{
+    [ValueConversion(typeof(Color), typeof(SolidColorBrush))]
+    public class ColorToSolidColorBrushConverter : IValueConverter
+    {
+        private SolidColorBrushToColorConverter _SolidColorBrushToColorConverter = new SolidColorBrushToColorConverter();
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return _SolidColorBrushToColorConverter.ConvertBack(value, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return _SolidColorBrushToColorConverter.Convert(value, targetType, parameter, culture);
+        }
+    }
+}
