91e08f6776fd15173c06cdd80ec913f4afce69b7
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-11-18 23:37:56 +0500

    EPasswordBox удален

--- CoreLand.UI/CustomControls/ExtendedControls/Consts/EPasswordBoxStrings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CoreLand.UI.CustomControls.ExtendedControls
-{
-    public class EPasswordBoxStrings
-    {
-        public const string ButtonXAMLName = "PART_IsPasswordMaskedButton";
-    }
-}
