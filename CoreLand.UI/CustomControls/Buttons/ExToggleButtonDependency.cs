cf7e724f73d263699c03ed9d2a87b7631cb5aa9d
Oscallo.developer@gmail.com <Oscallo.developer@gmail.com>
2023-11-24 21:30:34 +0500

    ExToggleButton

---
+++ CoreLand.UI/CustomControls/Buttons/ExToggleButtonDependency.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace CoreLand.UI.CustomControls.Buttons
+{
+
+    public partial class ExToggleButton : ToggleButton
+    {
+        public static readonly DependencyProperty EllipseBrushProperty = DependencyProperty.Register(nameof(EllipseBrush), typeof(Brush), typeof(ExToggleButton),
+                                   new FrameworkPropertyMetadata((Brush)null, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.SubPropertiesDoNotAffectRender));
+
+        static ExToggleButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ExToggleButton), new FrameworkPropertyMetadata(typeof(ExToggleButton)));
+        }
+
+        private static bool IsWidthHeightValid(object value)
+        {
+            double v = (double)value;
+            return (Double.IsNaN(v)) || (v >= 0.0d && !Double.IsPositiveInfinity(v));
+        }
+    }
+}
---
+++ CoreLand.UI/CustomControls/Buttons/ExToggleButtonExemplar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace CoreLand.UI.CustomControls.Buttons
+{
+
+    public partial class ExToggleButton : ToggleButton
+    {
+        [Bindable(true), Category("Appearance")]
+        public Brush EllipseBrush
+        {
+            get { return (Brush)GetValue(EllipseBrushProperty); }
+            set { SetValue(EllipseBrushProperty, value); }
+        }
+    }
+}
